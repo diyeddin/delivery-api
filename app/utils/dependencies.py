0a278ef24ecaadc681d0fc83d5030d57c011b011
diyeddin <40895033+diyeddin@users.noreply.github.com>
2025-10-01 20:32:38 +0300

    Modify role checking logic

--- app/utils/dependencies.py
+++ app/utils/dependencies.py
@@ -4,6 +4,7 @@ from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
 from app.core import security
 from app.db import models, database
+from typing import List
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 
@@ -25,9 +26,12 @@ def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(
         raise credentials_exception
     return user
 
-def require_role(required_roles):
+def require_role(allowed_roles: List[models.UserRole]):
     def role_checker(current_user: models.User = Depends(get_current_user)):
-        if current_user.role not in required_roles:
-            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not enough permissions")
+        if current_user.role not in allowed_roles:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail=f"Access forbidden. Requires one of: {', '.join([r.value for r in allowed_roles])}"
+            )
         return current_user
     return role_checker
